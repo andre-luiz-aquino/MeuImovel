361a8148504d6dda062edc89154dca5ecf5ea3c2
André Aquino <andre.luiz.aquino30@gmail.com>
2023-09-22 20:44:23 -0300

    Primeiro Commit

---
+++ src/python/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+
+link = "https://www.vivareal.com.br/aluguel/espirito-santo/serra/"
+requisicao = requests.get(link)
+soup  = BeautifulSoup(requisicao.text,"html.parser")
+tree = html.fromstring(str(soup))
+
+
+elementos_a = soup.select('<a')
+
+# Itere sobre os elementos <a> e imprima seus textos
+for elemento in elementos_a:
+    print(elemento)
+    
